8e741350975180043a89f07ad387a3653dbf59ff
ReeceRenninger <renningerreece@gmail.com>
2023-08-14 22:06:15 -0700

    updating content

--- src/Footer.js
+++ src/Footer.js
@@ -4,16 +4,22 @@ import { BsLinkedin } from "react-icons/bs";
 import { AiOutlineCopyright } from "react-icons/ai";
 import { Container } from 'react-bootstrap';
 
+const handleAnchorClick = (event) => {
+  event.preventDefault(); // Prevent the default link behavior
+  const url = event.currentTarget.href;
+  window.open(url, '_blank', 'noopener,noreferrer'); // Open link in new tab
+};
+
 class Footer extends React.Component{
   render(){
     return(
       <>
       <Container id='footer'>
       <h4><AiOutlineCopyright />Reece Renninger</h4>
-      <a href='https://github.com/ReeceRenninger'>
+      <a href='https://github.com/ReeceRenninger' onClick={handleAnchorClick}>
       <GoMarkGithub />
       </a>
-      <a href='https://www.linkedin.com/in/reecerenninger/'>
+      <a  href='https://www.linkedin.com/in/reecerenninger/' onClick={handleAnchorClick}>
       <BsLinkedin />
       </a>
       </Container>
